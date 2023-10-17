6034d2818119942e284f3d968df06da5e04e867e
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2023-10-17 19:05:09 +0530

    fetch API

--- Asynchronous JavaScript/ConsumePromise.js
+++ Asynchronous JavaScript/ConsumePromise.js
@@ -1,1 +1,9 @@
 // How to consume a Promise
+
+// Fetch() is an API given by the browser to us to make an EXTERNAL CALL and it returns a promise.
+
+const GITHUB_API = "https://api.github.com/users/akshaymarch7";
+
+const users = fetch(GITHUB_API);
+
+console.log(users);
