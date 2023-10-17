03d9869ebbfd24a881e581ca116db57ddba77045
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2023-10-17 19:36:09 +0530

    theory concepts of promise

--- Asynchronous JavaScript/ConsumePromise.js
+++ Asynchronous JavaScript/ConsumePromise.js
@@ -16,3 +16,15 @@ users
   .then(function (json) {
     console.log(json);
   });
+
+// Promise is an object representing the eventual completion or failure of an asynchronous operation.
+
+// Promises are used to handle the asynchronous operations in JavaScript.
+
+// Asynchronous operation are those operations which take some time to execute. We as a developer do not know how much time it takes to execute.
+
+// As soon as these operations are executed, it will return a promise.
+
+// A promise is an object which initially has empty data, as soon as we have data after execution of an asynchronous operation, promise object is filled with appropriate data.
+
+// We attach the then() handler with promise object and this then() handler takes a callback function and the response of promise object is passed to the parameter to the callback function.
