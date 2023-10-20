da88a9014c73f3f7137ff59a48c724aac5e881f1
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2023-10-20 23:24:03 +0530

    Another way of handling errors in async-await

--- Asynchronous JavaScript/Real-Examples.js
+++ Asynchronous JavaScript/Real-Examples.js
@@ -1,16 +1,14 @@
-const API_URL = "https://jsonplaceholder.typicodes.com/users";
+const API_URL = "https://jsonplaceholder.typicode.com/users";
 
 async function handlePromise() {
-  try {
-    //   console.log(fetch(API_URL));
-    const response = await fetch(API_URL);
-    //   console.log(response);
-    //   console.log(response.json());
-    const jsonData = await response.json();
-    console.log(jsonData);
-  } catch (error) {
-    console.log(error.message);
-  }
+  //   console.log(fetch(API_URL));
+  const response = await fetch(API_URL);
+  //   console.log(response);
+  //   console.log(response.json());
+  const jsonData = await response.json();
+  console.log(jsonData);
 }
-
-handlePromise();
+// console.log(handlePromise());
+handlePromise().catch(function (error) {
+  console.log(error.message);
+});
