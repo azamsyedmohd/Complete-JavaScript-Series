035ecd43c81eb67635f167e06049dd30d9e8d83c
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2023-10-20 23:00:34 +0530

    Real Example How fetch() is used with asyn-await

---
+++ Asynchronous JavaScript/Real-Examples.js
@@ -0,0 +1,12 @@
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+async function handlePromise() {
+  //   console.log(fetch(API_URL));
+  const response = await fetch(API_URL);
+  //   console.log(response);
+  //   console.log(response.json());
+  const jsonData = await response.json();
+  console.log(jsonData);
+}
+
+handlePromise();
