f4503dfcb74a1702cab2792d79ac5ac1f33686df
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2023-10-20 23:42:58 +0530

    Comments about async-await usage

--- Asynchronous JavaScript/Real-Examples.js
+++ Asynchronous JavaScript/Real-Examples.js
@@ -12,3 +12,12 @@ async function handlePromise() {
 handlePromise().catch(function (error) {
   console.log(error.message);
 });
+
+// async-await is combination to handle the asynchronous opertions in JavaScript.
+
+// async is a function which returns a promise either you returns a promise otherwise async() function itself returns.
+
+// await is only used in async() function to handle the promises.
+// by using await we directly get the promise result not the whole promise object and then we can also use for further usage of async operations.
+
+// If there is an error then we can handle by using try and catch block inside the async function or outside the async function at the time of calling with the catch() handler.
