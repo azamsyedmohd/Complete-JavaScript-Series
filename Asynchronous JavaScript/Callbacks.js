3316b92ca03af0ce0dab4e93a3d65d20f180c1a5
SYED MOHD AZAM <112909412+Syed-Mohd-Azam@users.noreply.github.com>
2023-10-21 23:44:13 +0530

    Callback Hell

--- Asynchronous JavaScript/Callbacks.js
+++ Asynchronous JavaScript/Callbacks.js
@@ -1,9 +1,21 @@
 // Callback function
 
-console.log("JavaScript");
+const cart = ["shoes", "pants", "jeans"];
 
-setTimeout(function () {
-  console.log("Callback function called!");
-}, 10000);
+// createOrder() is an API to create an order and it is async operation, which returns orderId.
 
-console.log("React Library");
+// proceedToPayment(orderId) is another API and another async opeartion which takes orderId as a parameter and do some payment operations.
+
+createOrder(cart, function (orderId) {
+  proceedToPayment(function (paymentInfo) {
+    showOrderSummary();
+  });
+});
+
+// We just passed the callback function to createOrder API and now it is responsibility of createOrder API to create orderId first and then execute this callback function.
+
+// Similarly, now this is a job of proceedToPayment to just do some payment related tasks and then call the callback function and so on.
+
+// This is one way to handle the asynchronous tasks via callback functions.
+
+//
