631282481c8a72af0b7449005cd99007e30a4fbc
Syed Mohd Azam <smazamamu@gmail.com>
2023-10-29 00:31:04 +0530

    GITHUB API Corrected

--- Asynchronous JavaScript/ConsumePromise.js
+++ Asynchronous JavaScript/ConsumePromise.js
@@ -2,7 +2,7 @@
 
 // Fetch() is an API given by the browser to us to make an EXTERNAL CALL and it returns a promise.
 
-const GITHUB_API = "https://api.github.com/users/akshaaymarch7";
+const GITHUB_API = "https://api.github.com/users/akshaymarch7";
 
 const users = fetch(GITHUB_API);
 
