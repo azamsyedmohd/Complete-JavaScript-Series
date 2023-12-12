0050973b50654c037cb6782a90f320a5822944ba
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-12 16:09:29 +0530

    Dot notation and Bracket Notation

--- Objects/ExamplesObjects.js
+++ Objects/ExamplesObjects.js
@@ -8,3 +8,18 @@ var person = {
 };
 
 console.log(person);
+
+// Dot notation
+
+console.log(person.age);
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.ownsCars);
+console.log("***************");
+
+// Bracket Notation
+
+console.log(person["age"]);
+console.log(person["firstName"]);
+console.log(person["lastName"]);
+console.log(person["ownsCars"]);
