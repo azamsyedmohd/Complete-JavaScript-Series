1ab23dbcf857786398f44b381df99bbc1c5aaa8b
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-12 16:19:13 +0530

    more example added

--- Objects/ExamplesObjects.js
+++ Objects/ExamplesObjects.js
@@ -15,9 +15,21 @@ console.log(person.age);
 console.log(person.firstName);
 console.log(person.lastName);
 console.log(person.ownsCars);
+
 console.log("***************");
 
 // Bracket Notation
+
+var age = "age";
+var firstName = "firstName";
+var lastName = "lastName";
+var ownsCars = "ownsCars";
+console.log(person[age]);
+console.log(person[firstName]);
+console.log(person[lastName]);
+console.log(person[ownsCars]);
+
+console.log("*********************");
 
 console.log(person["age"]);
 console.log(person["firstName"]);
