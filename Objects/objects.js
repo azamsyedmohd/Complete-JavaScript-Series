115cca4fb89e9de8c708589f1d7ab480b91fe9c8
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-12 15:41:13 +0530

    theory of objects

---
+++ Objects/objects.js
@@ -0,0 +1,3 @@
+// In JavaScript, an object is an unordered collection of key value pairs.Each key-value pair is called property.
+// The key of a property can be a string whereas the value of a property can be any value example string, number, array, object, function etc.
+// JavaScript provides you many ways to  create an object and commonly used one is object literal.
