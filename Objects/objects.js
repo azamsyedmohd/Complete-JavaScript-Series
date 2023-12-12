f642b41eddb81ce9fa8a790d440d587079006384
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-12 15:49:06 +0530

    Example of an object

--- Objects/objects.js
+++ Objects/objects.js
@@ -1,3 +1,12 @@
 // In JavaScript, an object is an unordered collection of key value pairs.Each key-value pair is called property.
 // The key of a property can be a string whereas the value of a property can be any value example string, number, array, object, function etc.
 // JavaScript provides you many ways to  create an object and commonly used one is object literal.
+
+var person = {
+  firstName: "Syed",
+  middleName: "Mohd",
+  lastName: "Azam",
+  age: 28,
+};
+
+// Here person is an object whereas the firstName,middleName,lastName,age are the keys and "Syed", "Mohd","Azam", 28 are the values.
