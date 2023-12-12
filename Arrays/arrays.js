d53d7e5afd71685f018a9934ba06119b35705dc4
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-12 18:11:02 +0530

    Array Example created

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,3 +1,14 @@
 // Similar to variable, an array also stores data but it is used to store multiple data items.
 // It enables storing multiple values under same variable name and we can perform various operations on them by using different methods and properties also every value gets a sequential index starting at 0.
 // In JavScript, arrays can store values of different types whereas in statically typed language array will store such type of values, that type a arry has.
+
+var arrayExample = [
+  24,
+  "Ferrari",
+  true,
+  null,
+  undefined,
+  ["Syed", "Mohd", "Azam"],
+];
+
+console.log(arrayExample);
