0ed29dd45153477602b12e2c937942fe26eca54a
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-12 18:26:57 +0530

    push( ) method of array

--- Arrays/MethodsArrays.js
+++ Arrays/MethodsArrays.js
@@ -4,3 +4,8 @@ console.log(values.pop());
 console.log(values);
 
 // pop() method removes and returns the last element from array.
+
+console.log(values.push(100)); // 4 total number of new elements.
+console.log(values);
+
+// push() method adds a new Element and returns the total number of new elements.
