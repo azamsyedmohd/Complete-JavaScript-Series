d39c6c80eb408855f10eafcdccff780f4e30ca30
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-13 18:34:25 +0530

    Basics of Functions

---
+++ Functions/functions.js
@@ -0,0 +1,23 @@
+// Functions
+// A function is a block of code that performs a specific task.
+// A JavaScript function is executed whenever it is invoked.
+// Functions are the building blocks in JavaScript.
+
+// Declaring and Invoking a function
+
+function functionName{
+    // whole code of a function comes inside this block
+} // This is how we declare the function. 
+
+functionName() // This is how we invoke the function.
+
+// Parameters and Arguments
+
+// A function can be defined with a set of parameters that it will take parameters are generally the values that we want the function to accept.
+
+// Arguments are the values that we pass for those parameters.
+
+function add(a,b){
+    console.log(a+b);
+}
+add(3,4); // Arguments
