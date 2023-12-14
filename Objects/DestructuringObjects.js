8de7805dd78b186293072d6b78270af670683d51
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-14 17:55:22 +0530

    Cloning an Object

--- Objects/DestructuringObjects.js
+++ Objects/DestructuringObjects.js
@@ -22,3 +22,79 @@ console.log(ageOfObject);
 console.log(genderOfObject);
 console.log(countryOfObject);
 console.log(cityOfObject);
+
+// Examples of destructuring of objects
+console.log("***************************");
+
+const exampleOne = {
+  name: "Alex",
+  address: "15th Park Avenue",
+  age: 43,
+};
+
+const { name, age, address } = exampleOne;
+
+console.log(name);
+console.log(address);
+console.log(age);
+
+// Example of Nested Destructuring
+
+const exampleTwo = {
+  name: "Alex",
+  address: "15th Park Avenue",
+  age: 43,
+  department: {
+    name: "Sales",
+    Shift: "Morning",
+    address: {
+      city: "Bangalore",
+      street: "7th Residency Rd",
+      zip: 560001,
+    },
+  },
+};
+
+const {
+  name: nameOfExampleTwo,
+  address: addressOfExampleTwo,
+  age: ageOfExampleTwo,
+  department: {
+    name: nameOfDepartment,
+    Shift: ShiftOfDepartment,
+    address: {
+      city: cityOfDepartment,
+      street: streetOfDepartment,
+      zip: zipOfDepartment,
+    },
+  },
+} = exampleTwo;
+
+console.log("*************************************");
+console.log(nameOfExampleTwo);
+console.log(addressOfExampleTwo);
+console.log(ageOfExampleTwo);
+console.log(nameOfDepartment);
+console.log(ShiftOfDepartment);
+console.log(cityOfDepartment);
+console.log(streetOfDepartment);
+console.log(zipOfDepartment);
+
+console.log("******************");
+
+const exampleClone = {
+  name: "Syed Mohd Azam",
+  qualification: "MCA",
+  age: 27,
+  address: {
+    country: "India",
+    city: "Aligarh",
+    university: "Aligarh Muslim University",
+    department: "Computer Science Department",
+  },
+};
+
+const referenceObject = exampleClone;
+const cloneObject = { ...exampleClone };
+console.log(referenceObject);
+console.log(cloneObject);
