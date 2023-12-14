53d2941de928b9b939f15ee96b864cc768ecb832
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-14 16:55:11 +0530

    Example of destructuring of array

--- Arrays/DestructuringArrays.js
+++ Arrays/DestructuringArrays.js
@@ -14,3 +14,32 @@ const [first, ...rest] = example;
 
 console.log(first);
 console.log(rest);
+
+const [
+  firstElement,
+  secondElement,
+  thirdElement,
+  fourthElement,
+  ...fifthElement
+] = ["HTML", "CSS", "JavaScript", "React"];
+
+console.log(firstElement);
+console.log(secondElement);
+console.log(thirdElement);
+console.log(fourthElement);
+console.log(fifthElement);
+
+// Examples of destructuring of elements.
+console.log("****************");
+const arrayOne = ["JavaScript", "React", "Front End Development", 27, true];
+const [one, two] = arrayOne;
+console.log(one);
+console.log(two);
+
+const [elemOne, elemTwo, ...rem] = arrayOne;
+console.log(elemOne); // 'JavaScript'
+console.log(elemTwo); // 'React'
+console.log(rem); // ['Front End Development',27,true]
+console.log("****************");
+const newArray = [...arrayOne, "Extra Element"];
+console.log(newArray);
