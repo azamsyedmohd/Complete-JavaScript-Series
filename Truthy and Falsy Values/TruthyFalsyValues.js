32fbc601361804683643863554a1e00c03b1ce7b
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-15 22:16:25 +0530

    Numbers to check truthy and falsy values

---
+++ Truthy and Falsy Values/TruthyFalsyValues.js
@@ -0,0 +1,12 @@
+// Truthy and Falsy Values
+
+// We can check what are the truthy and falsy values by using Boolean() function which takes one parameter and returns the corresponding boolean values.
+
+// Numbers
+console.log(Boolean(78)); // true
+console.log(Boolean(-78)); // true
+console.log(Boolean(0)); // false
+console.log(Boolean(-0)); //false
+console.log(Boolean(Infinity)); //true
+console.log(Boolean(-Infinity)); //true
+console.log(Boolean(NaN)); // false
