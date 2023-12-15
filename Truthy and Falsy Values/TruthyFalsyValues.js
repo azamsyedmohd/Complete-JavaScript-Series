02212d9804384a4dd44083c955678cf61ff55b80
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-15 22:22:35 +0530

    To check truthy and falsy values in String values

--- Truthy and Falsy Values/TruthyFalsyValues.js
+++ Truthy and Falsy Values/TruthyFalsyValues.js
@@ -3,6 +3,7 @@
 // We can check what are the truthy and falsy values by using Boolean() function which takes one parameter and returns the corresponding boolean values.
 
 // Numbers
+console.log("**************************");
 console.log(Boolean(78)); // true
 console.log(Boolean(-78)); // true
 console.log(Boolean(0)); // false
@@ -10,3 +11,15 @@ console.log(Boolean(-0)); //false
 console.log(Boolean(Infinity)); //true
 console.log(Boolean(-Infinity)); //true
 console.log(Boolean(NaN)); // false
+
+// Strings
+
+console.log("*****************************");
+console.log(Boolean("")); // false
+console.log(Boolean("Syed Mohd Azam")); // true
+console.log(Boolean("NaN")); // true
+console.log(Boolean("Infinity")); // true
+console.log(Boolean("-Infinity")); // true
+console.log(Boolean("78")); // true
+console.log(Boolean("-56")); // true
+console.log(Boolean("[]")); // true
