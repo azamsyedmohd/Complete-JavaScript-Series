21869e8f7795643613674e18db1730a97441b8c4
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-16 00:24:17 +0530

    Primitive Data types

---
+++ Primitive and Non-Primitive Data Types/PrimitiveDataTypes.js
@@ -0,0 +1,22 @@
+// Primitive Data Types
+
+// Primitive Data Types --> string,numbers,null,undefined,boolean values.
+
+// Actually primitive data types values are those values which can be stored directly into the varaiable (memory allocation). Since, the variables are used as containers to store the values. Actually, all the variables declared inrto the memory are constructed into the stack.
+
+// For example :-
+
+const number = 34;
+console.log(number);
+
+const string = "Syed Mohd Azam";
+console.log(string);
+
+const bool = true;
+console.log(bool);
+
+const emptyValueFirst = null;
+console.log(emptyValueFirst);
+
+const emptyValueSecond = undefined;
+console.log(emptyValueSecond);
