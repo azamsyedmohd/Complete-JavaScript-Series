e39e206f9c989c8f87a96b25977848c053923f74
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-16 17:32:34 +0530

    typeof  opearator

---
+++ Primitive and Non-Primitive Data Types/typeofOperator.js
@@ -0,0 +1,20 @@
+// typeof operator
+
+const num = 34;
+console.log(typeof num, num);
+const str = "Software Engineer";
+console.log(typeof str, str);
+const bool = true;
+console.log(typeof bool, bool);
+const obj = {
+  name: "Syed Mohd Azam",
+  age: 27,
+  designation: "Software Engineer",
+};
+console.log(typeof obj, obj);
+const arr = ["JavaSctipt", "TypeScript", "React", "Next"];
+console.log(typeof arr, arr);
+const func = function () {
+  console.log("Anonymous Function");
+};
+console.log(typeof func, func);
