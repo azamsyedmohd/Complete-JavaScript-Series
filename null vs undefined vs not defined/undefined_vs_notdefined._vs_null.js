384aa06e99098a4d729bc4811e75088b0798d1e9
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-16 16:36:46 +0530

    null vs undefined vs not defined

---
+++ null vs undefined vs not defined/undefined_vs_notdefined._vs_null.js
@@ -0,0 +1,7 @@
+console.log(value); // undefined
+var value = 34;
+const emptyVariable = null;
+console.log(emptyVariable); // null
+console.log(amplifier); // amplifier is not efined
+
+// Note :-> The main difference between not defined , undefined and null is that whenever we did not declared any variable and we are trying to use the value of that variable then it gives the error that is "variable_name is not defined". And when we use the variable declared with var before initialisation then at that time the valueof that variable is undefined.And whenever we want to store the value which is indicating that the variable has no value then there is special empty value which is value itself but shows there is no value in the variable and that value is called null.
