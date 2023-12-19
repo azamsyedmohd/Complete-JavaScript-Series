aad8fa324f235b5e2ddd3f0242833dd8ece4fa96
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-19 17:49:51 +0530

    Functional Programming and its concepts

---
+++ Functional Programming/functionalProgramming.js
@@ -0,0 +1,12 @@
+// Functional Programming is a programming paradigm designed to handle functions in a better way in JS (mainly pure functions).
+// JavaScript is a multi-paradigm language.We can use object-oriented,procedural and functional programming paradigms all at the same time in JavScript.
+// Functional Programming has a few important concepts that we need to know and understand. By implementing these concepts in your applications, you will end up with more functional code. This will make a huge difference in your application making it more readable, usable and maintainable.
+
+// Key features of functional Programming
+
+// Imperative and Declarative way of writing code.
+// Pure functions and side effects.
+// Higher Order Functions.
+// Composition
+// Closures
+// Currying
