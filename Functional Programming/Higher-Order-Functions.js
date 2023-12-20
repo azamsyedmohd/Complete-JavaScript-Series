0c2ca7f26aa1c130639faba60c5379a907bf3abe
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-20 15:16:35 +0530

    Basic Example with simple way without higher order function

--- Functional Programming/Higher-Order-Functions.js
+++ Functional Programming/Higher-Order-Functions.js
@@ -1,3 +1,13 @@
 // Higher Order Functions
 
 // Higher Order Function is a function that receives a function as an argument or returns the function as output.
+
+const arrayOne = [1, 2, 3, 4, 5];
+
+const squaredArrayOne = [];
+
+for (const value of arrayOne) {
+  squaredArrayOne.push(value * value);
+}
+
+console.log(squaredArrayOne);
