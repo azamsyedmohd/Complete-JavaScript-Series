c244bfd3c0d82485f8869885616b5d777e1ee19e
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-25 00:12:44 +0530

    Synchronous vs Single Threaded language

--- Basics Of JavaScript/Execution-Context.js
+++ Basics Of JavaScript/Execution-Context.js
@@ -5,3 +5,9 @@
 // Variable Environment --> This is the environment where all the variables and functions of a program are stored. Here, all the variables and functions are stored in the key value pairs.
 
 // Code Environment --> This is the environment where the code of a program is executed. This component is also called as Thread of Execution.
+
+// JavaScript is a single threaded and synchronous language
+
+// JavaScript is a synchronous language, it means that it will execute one instruction at a time. Once that instruction is executed, then it will execute another instruction after previous instruction.
+
+// JavaScript is a single threaded language, it means that it will execute the instruction in a specific order.It will maintain the order, it cannot execute the instruction randomly, or it cannot execute the instruction which takes less that time over that instruction which takes higher time.
