61f0e3c6100d6a73a489345802ef1263b85add34
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-24 23:52:00 +0530

    How JS Works?

---
+++ Basics Of JavaScript/Execution-Context.js
@@ -0,0 +1,7 @@
+// Execution Context
+
+// Everything in JavaScript happens inside the Execution Context.Execution Context is like a container and it contains two components --> Variable Environment and Thread of Execution.
+
+// Variable Environment --> This is the environment where all the variables and functions of a program are stored. Here, all the variables and functions are stored in the key value pairs.
+
+// Code Environment --> This is the environment where the code of a program is executed. This component is also called as Thread of Execution.
