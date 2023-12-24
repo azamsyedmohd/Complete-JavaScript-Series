b08465b9e53e517256f490f5b52290485acf9431
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-25 02:24:32 +0530

    Call Stack and its definition

--- Basics Of JavaScript/Execution-Context.js
+++ Basics Of JavaScript/Execution-Context.js
@@ -2,12 +2,14 @@
 
 // Everything in JavaScript happens inside the Execution Context.Execution Context is like a container and it contains two components --> Variable Environment and Thread of Execution.
 
-// Variable Environment --> This is the environment where all the variables and functions of a program are stored. Here, all the variables and functions are stored in the key value pairs.
+// Memory Component --> This is the environment where all the variables and functions of a program are stored. Here, all the variables and functions are stored in the key value pairs.This is also known as Variable Environment.
 
-// Code Environment --> This is the environment where the code of a program is executed. This component is also called as Thread of Execution.
+// Code Component --> This is the environment where the code of a program is executed. This component is also called as Thread of Execution.
 
 // JavaScript is a single threaded and synchronous language
 
 // JavaScript is a single threaded language, it means that it will execute one instruction at a time. Once that instruction is executed, then it will execute another instruction after previous instruction.
 
 // JavaScript is a synchronous language, it means that it will execute the instruction in a specific order.It will maintain the order, it cannot execute the instruction randomly, or it cannot execute the instruction which takes less that time over that instruction which takes higher time.
+
+// Call Stack maintains the order of execution of execution contexts.
