b5d75156cac33b6729c9cd2c3e37b147faef8cf6
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-25 13:56:02 +0530

    JavaScript is a loosely typed language

--- null vs undefined vs not defined/undefined_vs_not-defined.js
+++ null vs undefined vs not defined/undefined_vs_not-defined.js
@@ -14,4 +14,16 @@ if (a === undefined) {
 } else {
   console.log("a is not undefined");
 }
-console.log(x); // x is not defined
+// console.log(x); // x is not defined
+
+// JavaScript is a loosely typed language
+
+console.log("**********************************");
+
+console.log(b);
+var b = 7;
+console.log(b); // 7
+b = "Front End Development";
+console.log(b); // "Front End Development"
+b = true;
+console.log(b); // true
