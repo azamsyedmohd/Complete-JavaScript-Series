946547fe13a107838662af8cc86af9658042f90e
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-25 17:01:06 +0530

    Hoisting done

--- Basics Of JavaScript/Hoisting.js
+++ Basics Of JavaScript/Hoisting.js
@@ -1,7 +1,11 @@
 // Hoisting
+console.log(getName);
+getName();
+console.log(x);
 var x = 7;
 function getName() {
   console.log("Namaste JavaScript");
 }
+console.log(getName);
 getName();
 console.log(x);
