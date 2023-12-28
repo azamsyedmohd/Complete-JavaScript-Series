0099fcfbe1c3589d504b75840158b5a73ff26064
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-28 18:44:45 +0530

    Shadowing Concepts

--- Basics Of JavaScript/Block-Scope_&_Shadowing.js
+++ Basics Of JavaScript/Block-Scope_&_Shadowing.js
@@ -4,6 +4,8 @@
 
 // Why we need a block? Basically whenever JavaScript Engine expects a single statement to execute but we want to execute multiple statements instead of single statement then we combine those statements inside the block.
 var a = 100;
+let b = 45;
+const c = 34;
 {
   var a = 10; // Global Variable
   let b = 20; // Local Variable
@@ -13,5 +15,6 @@ var a = 100;
   console.log(c); //30
 }
 console.log(a); // 10
-
+console.log(b); // 45
+console.log(c); // 34
 // This is known as Shadowing.
