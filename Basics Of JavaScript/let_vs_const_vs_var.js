ef43828f62d7a92d321e53c2a73d2b71f25f96bd
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-28 17:15:37 +0530

    const declaration

--- Basics Of JavaScript/let_vs_const_vs_var.js
+++ Basics Of JavaScript/let_vs_const_vs_var.js
@@ -10,8 +10,17 @@ a = "JavaScript";
 console.log(a);
 
 // let declaration
-console.log(b); // Reference Error: Cannot access before initialisation.
+// console.log(b); // Reference Error: Cannot access before initialisation.
 let b;
 console.log(b); // undefined
 b = 50;
 console.log(b); //50
+
+// variable declared with const keyword.
+
+// const c; // Syntax Error: Missing Initialiser with const keyword.
+// console.log(c); // Reference Error: Cannot access before initialisation.
+const c = 100;
+// const c = 45; // Syntax Error: Cannot redeclare variable with const keyword.
+c = 345; // TypeError : Assignment to const variable.
+console.log(c);
