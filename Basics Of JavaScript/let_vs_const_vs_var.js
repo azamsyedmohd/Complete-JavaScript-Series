6cc8fd85e3170e4250d30920094a5e9030afccab
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-28 16:42:35 +0530

    var declaration

---
+++ Basics Of JavaScript/let_vs_const_vs_var.js
@@ -0,0 +1,10 @@
+// let var and const declarations.
+
+// variable declaration with var keyword.
+console.log(a);
+var a;
+console.log(a);
+a = 25;
+console.log(a);
+a = "JavaScript";
+console.log(a);
