8e5a8d40aca7abfe0b7fdb63c5ce7dbe53f90370
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-28 16:21:44 +0530

    Second Case of Reference Error

--- Basics Of JavaScript/Let_&_const_temporalDeadZone.js
+++ Basics Of JavaScript/Let_&_const_temporalDeadZone.js
@@ -2,9 +2,13 @@
 
 // let and const declarations are hoisted but they are hoisted differently from var declarations. They are hoisted in temporal dead zone.
 console.log(b); // undefined
-console.log(a); // ReferenceError: cannot access before initialisation a.
+// console.log(a); // ReferenceError: cannot access before initialisation a.
 let a = 10;
 var b = 100;
 
 // Temporal ead zone is the time where let and constant variables are hoisted to the time where they are initialised.
+
 // Whenever you want to execute the variable declared with let and const during temporal dead zone it gives you the error called Reference Error.
+console.log(x); // Reference Error : x is not defined.
+
+// Second case is that whenever we are unable to find the variable in both temporal dead zone and global object then we say that variable is not defined and it comes under the Reference Error.
