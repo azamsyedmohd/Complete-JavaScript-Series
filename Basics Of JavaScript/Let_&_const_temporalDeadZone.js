6a34e817b6fed14a990ba490924b970a2c2ca3fc
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-28 16:06:53 +0530

    cannot access before initialisation in case of hoisting of let variable

--- Basics Of JavaScript/Let_&_const_temporalDeadZone.js
+++ Basics Of JavaScript/Let_&_const_temporalDeadZone.js
@@ -1,3 +1,7 @@
 // let and const in temporal dead zone.
 
 // let and const declarations are hoisted but they are hoisted differently from var declarations. They are hoisted in temporal dead zone.
+console.log(b); // undefined
+console.log(a); // ReferenceError: cannot access before initialisation a.
+let a = 10;
+var b = 100;
