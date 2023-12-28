18d226eef6ea31159f8dffd56283d2d9e1d71681
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-28 18:20:41 +0530

    Block definition and its need

---
+++ Basics Of JavaScript/Block-Scope_&_Shadowing.js
@@ -0,0 +1,5 @@
+// Block-Scope And Shadowing
+
+// Block is basically a pair of curly braces which is used to combine the compound statements.
+
+// Why we need a block? Basically whenever JavaScript Engine expects a single statement to execute but we want to execute multiple statements instead of single statement then we combine those statements inside the block.
