926bd931ce1ff85b5fb54001abb73965d6f9a218
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-28 17:03:15 +0530

    let declaration

--- Basics Of JavaScript/let_vs_const_vs_var.js
+++ Basics Of JavaScript/let_vs_const_vs_var.js
@@ -8,3 +8,10 @@ a = 25;
 console.log(a);
 a = "JavaScript";
 console.log(a);
+
+// let declaration
+console.log(b); // Reference Error: Cannot access before initialisation.
+let b;
+console.log(b); // undefined
+b = 50;
+console.log(b); //50
