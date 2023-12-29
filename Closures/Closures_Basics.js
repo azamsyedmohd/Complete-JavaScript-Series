4c801fd4d93c7a6a2fa73145c0a3beaaa6105201
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-29 22:58:02 +0530

    Closure example one

--- Closures/Closures_Basics.js
+++ Closures/Closures_Basics.js
@@ -1,3 +1,14 @@
 // Basics of Closure
 
 // Closure is basically refers to the combination of a function with the lexical environment of its parent. In other words, you can say that the function whenever executes outside of its lexical environment remembers its surroundings.
+
+function outer() {
+  var value = 20;
+  function inner() {
+    console.log(value);
+  }
+  value = 50;
+  return inner;
+}
+var close = outer();
+close();
