6830158f115046c6425bc162a1946cb674855f7c
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-29 18:34:13 +0530

    Depth understanding of Closure

--- Closures/Closures.js
+++ Closures/Closures.js
@@ -4,8 +4,12 @@ function x() {
   function y() {
     console.log(a);
   }
-  y();
+  a = 100;
+  return y;
 }
-x();
+var z = x();
+z(); // 100
 
 // A closure is the combination of a function bundled together (enclosed) with references to its surroundings state(lexical environment) of its parent.In other words, closure gives an access to the outer's function scope from an inner function.
+
+// This function y(){} makes a closure with x for the variable a. After executing so much lines of code even x() also been removed from call stack but y () remembers its closure or where it was present.
