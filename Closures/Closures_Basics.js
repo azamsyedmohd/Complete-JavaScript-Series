da654cc377ade7b4ad3534645c7c6685c81d54b4
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-29 22:44:52 +0530

    Closure Basics

---
+++ Closures/Closures_Basics.js
@@ -0,0 +1,3 @@
+// Basics of Closure
+
+// Closure is basically refers to the combination of a function with the lexical environment of its parent. In other words, you can say that the function whenever executes outside of its lexical environment remembers its surroundings.
