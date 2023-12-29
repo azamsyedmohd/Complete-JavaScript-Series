fac730b5297d3592bbe3c3839d2f4c11f8242f37
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-29 23:48:36 +0530

    New Objects created through Constructor Function

--- Closures/Constructor_Function_Closure.js
+++ Closures/Constructor_Function_Closure.js
@@ -10,8 +10,17 @@ function Counter() {
     console.log(count);
   };
 }
+console.log("*********************");
 var counter1 = new Counter();
 counter1.incrementCounter();
 counter1.decrementCounter();
 counter1.incrementCounter();
 counter1.decrementCounter();
+
+console.log("*************************");
+
+var counter2 = new Counter();
+counter2.incrementCounter();
+counter2.decrementCounter();
+counter2.incrementCounter();
+counter2.decrementCounter();
