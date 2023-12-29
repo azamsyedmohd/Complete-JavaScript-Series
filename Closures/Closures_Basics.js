1c992048ec0f88c673805127ee18e19b911dd5f2
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-29 22:58:58 +0530

    Another example still make a closure

--- Closures/Closures_Basics.js
+++ Closures/Closures_Basics.js
@@ -3,11 +3,10 @@
 // Closure is basically refers to the combination of a function with the lexical environment of its parent. In other words, you can say that the function whenever executes outside of its lexical environment remembers its surroundings.
 
 function outer() {
-  var value = 20;
   function inner() {
     console.log(value);
   }
-  value = 50;
+  let value = 50;
   return inner;
 }
 var close = outer();
