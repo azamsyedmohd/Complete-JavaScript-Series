5157491631f573bf5f576aa34f15944a6333f631
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-29 18:16:34 +0530

    Closure definition

--- Closures/Closures.js
+++ Closures/Closures.js
@@ -7,3 +7,5 @@ function x() {
   y();
 }
 x();
+
+// A closure is the combination of a function bundled together (enclosed) with references to its surroundings state(lexical environment) of its parent.In other words, closure gives an access to the outer's function scope from an inner function.
