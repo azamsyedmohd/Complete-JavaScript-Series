d3c9d9e67e406a32220878aaed72faec5d73eafb
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-29 23:10:03 +0530

    Example of Nested Closure

--- Closures/Closures_Basics.js
+++ Closures/Closures_Basics.js
@@ -2,12 +2,18 @@
 
 // Closure is basically refers to the combination of a function with the lexical environment of its parent. In other words, you can say that the function whenever executes outside of its lexical environment remembers its surroundings.
 
-function outer(param) {
-  function inner() {
-    console.log(param, value);
+function outest() {
+  var x = "HTML & CSS";
+  function outer(y) {
+    function inner() {
+      console.log(x);
+      console.log(y);
+      console.log(z);
+    }
+    let z = "React";
+    return inner;
   }
-  let value = "React";
-  return inner;
+  return outer;
 }
-var close = outer("JavaScript");
+var close = outest()("JavaScript");
 close();
