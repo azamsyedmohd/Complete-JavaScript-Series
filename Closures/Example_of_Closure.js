be383ee57e9b6d97ff7d97ae3db803a3c9b4dd5b
Syed-Mohd-Azam <smazamamu@gmail.com>
2023-12-30 18:21:35 +0530

    Closure explaination

--- Closures/Example_of_Closure.js
+++ Closures/Example_of_Closure.js
@@ -1,5 +1,7 @@
 // Example of Closure
 
+// Closure is the combination of a function with the lexical environment of its parent to access the variables and functions of the parent.In other words, the closure allows us to access the outside environment of the parent.
+
 function outer() {
   var firstValue = 25;
   var secondValue = "Google";
