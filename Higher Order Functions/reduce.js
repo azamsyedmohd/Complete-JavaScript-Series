97ae0b24993ef526e9625947e3714666afdb62df
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-04 02:39:46 +0530

    Maximum value among the values of an array using reduce method

--- Higher Order Functions/reduce.js
+++ Higher Order Functions/reduce.js
@@ -7,3 +7,11 @@ const third = [5, 1, 3, 2, 6];
 const sum = third.reduce(function (sum, value) {
   return sum + value;
 }, 0);
+
+// Finding the maximum of the values of an array
+
+const maximum = third.reduce(function (max, value) {
+  return max > value ? max : value;
+}, -Infinity);
+
+console.log("Maximum Value : " + maximum);
