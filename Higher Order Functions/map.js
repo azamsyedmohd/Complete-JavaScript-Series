b7ca80811b6733c5e1f2f66aa9e5613ff3ab45a5
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-04 00:56:59 +0530

    using map with first way

---
+++ Higher Order Functions/map.js
@@ -0,0 +1,26 @@
+// map method
+
+const first = [5, 1, 3, 2, 6];
+
+// Double -- [10,2,6,4,12]
+
+// Tripple -- [15,3,9,6,18]
+
+// Binary -- ["101","001","011","010","110"]
+
+// First Way
+const double = function (value) {
+  return value * 2;
+};
+
+const tripple = function (value) {
+  return value * 3;
+};
+
+const binary = function (value) {
+  return value.toString(2);
+};
+
+console.log(first, map(double));
+console.log(first.map(tripple));
+console.log(first.binary);
