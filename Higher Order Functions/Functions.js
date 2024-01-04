640183a7b0a88e96714b859ac533f588fcab92ed
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-04 21:48:52 +0530

    difference between function declaration and function expression

--- Higher Order Functions/Functions.js
+++ Higher Order Functions/Functions.js
@@ -15,3 +15,5 @@ var value = function () {
 };
 
 value();
+
+// The difference between function declaration and function expression is hoisting because whenever we declare the function using function declaration or function statement then the memory is assigned to the function initially before a single line of code is executed.But in the case of function expression, first of all the undefined value is provided to the variable until the declaration of function is executed through function expression.
