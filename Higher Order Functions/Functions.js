5dd4ac6ed9dc5be01a7cc5b75b178200899bc1b1
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-01-04 21:43:32 +0530

    Function Expression

--- Higher Order Functions/Functions.js
+++ Higher Order Functions/Functions.js
@@ -3,5 +3,15 @@
 // Function Statement and Function Declaration
 
 function language() {
-  console.log("JavaScript Language");
+  console.log("JavaScript Language!");
 }
+
+language();
+
+// Function Expression --> Anonymous function stored ina variable is called function expression.
+
+var value = function () {
+  console.log("Function Expression!");
+};
+
+value();
